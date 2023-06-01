930f3fe9395faafbb6c7610d55c1ec36dd94fb88
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-06-01 22:42:59 +0100

    widget update

--- src/views/settings/WidgetSet.vue
+++ src/views/settings/WidgetSet.vue
@@ -7,10 +7,22 @@
         <div class="head">
             <h3 class="mainText">Notes</h3>
             <i class="fa-sharp fa-regular fa-note-sticky icon-wid"></i>
+            <div class="switchFlex">
+                    <label class="switch" >
+                    <input type="checkbox"> 
+                    <span class="slider"></span>
+                    </label>
+                </div>
         </div>
         <div class="head">
             <h3 class="mainText">Todo List</h3>
             <i class="fa-solid fa-list icon-wid"></i>
+            <div class="switchFlex">
+                    <label class="switch" >
+                    <input type="checkbox" > 
+                    <span class="slider"></span>
+                    </label>
+                </div>
         </div>
     </div>
 </template>
@@ -22,7 +34,7 @@ export default {
   components: {
 
   },
- 
+
 };
 </script>
 
@@ -32,30 +44,84 @@ export default {
     background-color: #fff;
   padding: 20px 150px;
   color: rgb(5, 169, 219);
-  width: 110vh;
+  width: 80vh;
   height: 12vh;
-  margin-left: 21%;
+  margin-left: 28%;
   border: 1px solid #ccc;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
 }
 
 .fa-list{
-    margin-left: -12%;
+    margin-left: -16%;
     margin-top: 5%;
-    z-index: 2;
+   
 }
 
 .fa-note-sticky{
-    margin-left: -9%;
+    margin-left: -11%;
     margin-top: 5%;
-    z-index: 2;
+   
+}
+.mainText{
+    margin-left: -13%;
+    margin-top: 4.5%;
+   padding-left: 10px;
+
 }
 
+.switchFlex{
+    margin-left: 100%;
+    margin-top: 4%;
+}
 
-.mainText{
-    margin-left: -12%;
-    margin-top: 4.5%;
-    z-index: 2;
+/* The switch - the box around the slider */
+.switch {
+  font-size: 17px;
+  position: relative;
+  display: inline-block;
+  width: 3.5em;
+  height: 2em;
+}
 
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  box-shadow: inset 2px 5px 10px rgba(0,0,0,0.3);
+  transition: .4s;
+  border-radius: 5px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 1.4em;
+  width: 0.1em;
+  border-radius: 0px;
+  left: 0.3em;
+  bottom: 0.3em;
+  background-color: white;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #03741f;
+  box-shadow: inset 2px 5px 10px rgb(0, 0, 0);
+}
+
+input:checked + .slider:before {
+  transform: translateX(2.8em) rotate(360deg);
 }
 </style>
