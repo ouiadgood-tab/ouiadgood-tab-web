06aad1513c1636fdf35bc9c0611e2520475982a2
Original Timi <timileyinoyelekan11@gmail.com>
2023-06-15 03:17:46 +0000

    added admin route seperate

--- src/views/settings/Admin.vue
+++ src/views/settings/Admin.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container">
+    <h1>Admin</h1>
     <div class="setPos">
-      <SettingNav />
+      <a class="router" href="/setting/Admin">Create Charity</a>
+      <a class="router" href="/setting/Donate">Edit Charity</a>
+      <button @click="logoutUser">logout</button>
     </div>
     <div class="admin">
       <h1>Create Charity</h1>
       <div class="">
         <input v-model="name" req name="name" placeholder="Create Charity" />
         <input v-model="about" name="about" placeholder="About Charity" />
+        <input type="file" class="image-upload" name="image" placeholder="" />
         <br />
         <button @click="create">Create</button>
       </div>
@@ -16,11 +20,12 @@
 </template>
 
 <script>
-import SettingNav from "./SettingNav.vue";
+// import SettingNav from "./SettingNav.vue";
 import axios from "axios";
+import { googleLogout } from "vue3-google-login"
 export default {
   name: "AdminView",
-  components: { SettingNav },
+  // components: { SettingNav },   
   data() {
     return {
       name: "",
@@ -28,16 +33,32 @@ export default {
     };
   },
   methods: {
+    logoutUser() {
+      // Delete local storage data
+      localStorage.removeItem("loginRequest");
+    
+      // Redirect to /login
+      // this.$router.push('/login');
+      window.location.replace("/login");
+      googleLogout()
+    },
     create() {
+      const imageFile = document.querySelector('.image-upload').files[0];
+      console.log(imageFile)
+      const formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('about', this.about);
+    formData.append('image', imageFile || null);
       const pattern = /^$|^.{0,2}$/;
       if (pattern.test(this.name) || pattern.test(this.about)) {
         return alert("Length too Short");
       }
       // Make a POST request to the API endpoint
       axios
-        .post("https://ouiadgood.onrender.com/charity/add", {
-          name: this.name,
-          about: this.about,
+        .post("https://ouiadgood.onrender.com/charity/add",formData,{
+          headers: {
+        'Content-Type': 'multipart/form-data',
+      },
         })
         .then((response) => {
           if (response.status == 200) {
@@ -47,7 +68,7 @@ export default {
           }
         })
         .catch((err) => {
-          alert(err.response.data);
+         console.log(err)
         });
     },
   },
@@ -61,10 +82,5 @@ button {
   margin: 10px;
   padding: 20px;
 }
-.setPos {
-  position: static;
-}
-.admin {
-  margin-top: -45%;
-}
+
 </style>
--- src/views/settings/DonateView.vue
+++ src/views/settings/DonateView.vue
@@ -1,24 +1,54 @@
 <template>
-    <div>
-        <div class="setPos">
-            <SettingNav/>
-        </div>
-        <DonateHeart class="statsDown"/>
+  <div>
+    <div class="setPos">
+      <div v-show="!admin">
+        <SettingNav />
+      </div>
     </div>
+
+    <div v-show="admin">
+      <AdminNav />
+    </div>
+
+    <DonateHeart :class="!admin?'statsDown': '' " />
+  </div>
 </template>
 
 <script>
-import DonateHeart from './DonateHeart.vue';
-import SettingNav from './SettingNav.vue';
+import DonateHeart from "./DonateHeart.vue";
+import SettingNav from "./SettingNav.vue";
+import AdminNav from "@/components/AdminNav.vue";
+import { googleLogout } from "vue3-google-login"
 
-export default{
-    components: { SettingNav, DonateHeart }
-}
+export default {
+  components: { SettingNav, DonateHeart, AdminNav },
+  data() {
+    return {
+      admin: false,
+    };
+  },
+
+  methods:{
+    logoutUser() {
+      // Delete local storage data
+      localStorage.removeItem("loginRequest");
+    
+      // Redirect to /login
+      // this.$router.push('/login');
+      window.location.replace("/login");
+      googleLogout()
+    },
+  },
+  created() {
+    const getUser = JSON.parse(localStorage.getItem("loginRequest") || "{}");
+    this.admin = getUser.admin || false;
+  },
+};
 </script>
 
 <style scoped>
-.setPos{
-    position: static;
+.setPos {
+  position: static;
 }
 .statsDown{
     margin-top: -50%;
